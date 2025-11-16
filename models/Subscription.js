6335f4aada16e71de19d32f99d61339b6c6e9dbd
La Vũ Minh Triết <133770445+lavuminhtriet@users.noreply.github.com>
2025-11-17 00:45:18 +0700

    Sửa lỗi code

--- models/Subscription.js
+++ models/Subscription.js
@@ -2,32 +2,32 @@
 const mongoose = require('mongoose');
 
 const subscriptionSchema = new mongoose.Schema({
-  // Token của thiết bị
+
   pushToken: {
     type: String,
     required: true,
     unique: true,
   },
   
-  // (Module 2.2) Ngưỡng AQI
+
   alertThreshold: {
     type: Number,
     default: 150,
   },
   
-  // (Module 2.2) Vị trí để kiểm tra AQI
+
   defaultLocation: {
     type: String,
     required: true,
   },
 
-  // (MỚI) DÀNH CHO MODULE 6.2 (Nhắc rác)
+
   recycleReminderEnabled: {
     type: Boolean,
     default: false,
   },
   recycleReminderDay: {
-    type: String, // Sẽ lưu 'T2', 'T3', 'T6', v.v.
+    type: String, 
     default: null,
   },
   
